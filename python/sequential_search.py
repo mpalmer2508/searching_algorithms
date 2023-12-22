3fd8d98f80176170297c59bfc8b82593831886bb
Matthew Palmer <palmermatthew645@gmail.com>
2023-12-22 07:50:53 -0700

    Add docstring to sequential_search()

--- python/sequential_search.py
+++ python/sequential_search.py
@@ -1,4 +1,14 @@
 def sequential_search(values, requested_value):
+    """
+    Perform a sequential search to find the index of the requested value in a list.
+
+    Parameters:
+    - values (list): The list of values to be searched.
+    - requested_value: The target value to be searched for.
+
+    Returns:
+    - int: The index of the requested value if found, or -1 if not found.
+    """
 
     for i in range(len(values)):
         if values[i] == requested_value:
