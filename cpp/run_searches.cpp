69fbb6ce396ddff1afca8747a6b902c5046f39c1
Matthew Palmer <palmermatthew645@gmail.com>
2024-01-04 12:29:34 -0700

    Added cpp files

---
+++ cpp/run_searches.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+// void run_search(function search_alg, values, x) {
+//     """
+//     Run a search algorithm on a list of values to find the target element.
+
+//     Parameters:
+//     - search_alg (function): The search algorithm function to be used.
+//     - values (list): The list of values to be searched.
+//     - x: The target element to be searched for.
+
+//     Returns:
+//     - None: Prints the result of the search along with the time taken.
+//     """
+//     start = time.perf_counter()
+//     found = search_alg(values, x)
+//     finish = time.perf_counter()
+//     run_time = finish - start
+//     print_string = ""
+//     if found != -1:
+//         print_string += f"{search_alg.__name__:<30} {x} was found at index {found} ("
+//     else:
+//         print_string += f"{search_alg.__name__:<30} {x} was not found ("
+
+//     print_string += convert_find_time(run_time)
+//     print(print_string)
+// }
+
+int main() {
+    int values[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int x = 3;
+    
+
+}
